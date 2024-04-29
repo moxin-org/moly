1310ec63051e3d642c62016b23a0b2a4849f5e0f
Julian Montes de Oca <julian@wyeworks.com>
2024-04-29 10:50:45 -0300

    Centralize local files in a home dir

---
+++ moxin-frontend/src/data/filesystem.rs
@@ -0,0 +1,33 @@
+use std::path::PathBuf;
+use std::{env, fs};
+
+// Note that .moxin will create a hidden folder in unix-like systems.
+// However in Windows the folder will be visible by default.
+pub const DEFAULT_DOWNLOADS_DIR: &str = ".moxin/model_downloads";
+pub const MOXIN_HOME_DIR: &str = ".moxin";
+
+pub fn setup_model_downloads_folder() -> String {
+    let home_dir = get_home_dir();
+    let downloads_dir = PathBuf::from(home_dir).join(DEFAULT_DOWNLOADS_DIR);
+
+    if fs::create_dir_all(&downloads_dir).is_err() {
+        eprintln!(
+            "Failed to create the model downloads directory at '{}'. Using current directory as fallback.",
+            downloads_dir.display()
+        );
+        ".".to_string()
+    } else {
+        downloads_dir.to_string_lossy().to_string()
+    }
+}
+
+pub fn get_home_dir() -> String {
+    env::var("HOME"). // Unix-like systems
+        or_else(|_| env::var("USERPROFILE")) // Windows
+        .unwrap_or_else(|_| ".".to_string())
+}
+
+pub fn moxin_home_dir() -> PathBuf {
+    let home_dir = get_home_dir();
+    PathBuf::from(home_dir).join(MOXIN_HOME_DIR)
+}
--- moxin-frontend/src/data/mod.rs
+++ moxin-frontend/src/data/mod.rs
@@ -1,5 +1,6 @@
 pub mod chat;
 pub mod download;
+pub mod filesystem;
 pub mod preferences;
 pub mod search;
 pub mod store;
