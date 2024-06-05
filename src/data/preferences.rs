ce996c2cc306434b1ba4d307e8ea1d4233d7b2e3
Jorge Bejar <jorge@wyeworks.com>
2024-06-05 18:16:02 -0300

    Refactor Preferences serde default value for models path

--- src/data/preferences.rs
+++ src/data/preferences.rs
@@ -10,10 +10,10 @@ use serde::{Deserialize, Serialize};
 use super::filesystem::{project_dirs, setup_model_downloads_folder};
 const PREFERENCES_FILENAME: &str = "preferences.json";
 
-#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct Preferences {
     pub current_chat_model: Option<FileID>,
-    #[serde(default = "setup_model_downloads_folder")]
+    #[serde(default)]
     pub downloaded_files_dir: PathBuf,
 }
 
@@ -21,7 +21,8 @@ impl Preferences {
     pub fn load() -> Self {
         match read_from_file() {
             Ok(json) => {
-                let preferences: Preferences = serde_json::from_str(&json).unwrap();
+                let mut preferences: Preferences = serde_json::from_str(&json).unwrap();
+                preferences.downloaded_files_dir = setup_model_downloads_folder();
                 return preferences;
             }
             Err(_) => {}
