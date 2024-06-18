c31eddc2bdfae49115cd88ba572a4dbb539c8f50
Kevin Boos <kevinaboos@gmail.com>
2024-06-18 14:36:44 -0700

    remove debugging printout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
 fn main() {
-    println!("------------------------- Environment Variables -------------------------");
-    println!("{:#?}", std::env::vars().collect::<Vec<_>>());
-
     moxin::app::app_main()
 }
