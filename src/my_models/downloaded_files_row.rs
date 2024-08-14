e2a27afc15d8ae357a43c929851fd6b94e222750
noxware <7684329+noxware@users.noreply.github.com>
2024-08-14 12:00:07 -0300

    remove leftover reference to play_arrow.svg

--- src/my_models/downloaded_files_row.rs
+++ src/my_models/downloaded_files_row.rs
@@ -15,7 +15,6 @@ live_design! {
     import crate::my_models::delete_model_modal::DeleteModelModal;
 
     ICON_START_CHAT = dep("crate://self/resources/icons/start_chat.svg")
-    ICON_PLAY = dep("crate://self/resources/icons/play_arrow.svg")
     ICON_INFO = dep("crate://self/resources/icons/info.svg")
     ICON_DELETE = dep("crate://self/resources/icons/delete.svg")
     MODEL_CTA_COLOR = #127487
