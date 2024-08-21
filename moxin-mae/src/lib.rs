1577a87610e8bd25f2009d7f090dd14b77a60bce
Jorge Bejar <jorge@wyeworks.com>
2024-08-21 11:03:46 -0300

    Refactor MAE backend to have only one loop

--- moxin-mae/src/lib.rs
+++ moxin-mae/src/lib.rs
@@ -5,10 +5,10 @@ use dora_node_api::{
     DoraNode, Event, MetadataParameters,
 };
 use eyre::Context;
-use serde_json::Value;
 use serde::{Deserialize, Serialize};
-use std::sync::mpsc::{self, channel};
+use serde_json::Value;
 use std::collections::HashMap;
+use std::sync::mpsc::{self, channel};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MaeResponseQuestioner {
@@ -25,21 +25,21 @@ pub struct MaeResponseWebSearch {
 #[derive(Debug, Copy, Clone)]
 pub enum MaeAgent {
     Questioner,
-    WebSearch
+    WebSearch,
 }
 
 impl MaeAgent {
     pub fn name(&self) -> String {
         match self {
             MaeAgent::Questioner => "Questioner".to_string(),
-            MaeAgent::WebSearch => "WebSearch".to_string()
+            MaeAgent::WebSearch => "WebSearch".to_string(),
         }
     }
 
     pub fn definition_file(&self) -> String {
         match self {
             MaeAgent::Questioner => "reasoner_agent.yml".to_string(),
-            MaeAgent::WebSearch => "web_search_by_dspy.yml".to_string()
+            MaeAgent::WebSearch => "web_search_by_dspy.yml".to_string(),
         }
     }
 
@@ -79,90 +79,85 @@ impl MaeBackend {
 
     pub fn new() -> Self {
         let (command_sender, command_receiver) = channel();
-        let (tx, rx) = channel();
         let backend = Self { command_sender };
 
         std::thread::spawn(move || {
-            Self::start_receiver_loop(tx);
-        });
-
-        std::thread::spawn(move || {
-            Self::start_sender_loop(command_receiver, rx);
+            Self::main_loop(command_receiver);
         });
 
         backend
     }
 
-    pub fn start_receiver_loop(sender: mpsc::Sender<String>) {
-        loop {
-            if let Ok((_node, mut events)) =
-                DoraNode::init_from_node_id(NodeId::from("reasoner_output_moxin".to_string()))
-            {
-                while let Some(event) = events.recv() {
-                    match event {
-                        Event::Input {
-                            id,
-                            metadata: _,
-                            data,
-                        } => match data.data_type() {
-                            dora_node_api::arrow::datatypes::DataType::Utf8 => {
-                                let received_string: &str = TryFrom::try_from(&data)
-                                    .context("expected string message")
-                                    .expect("expected string message");
+    pub fn main_loop(command_receiver: mpsc::Receiver<MaeAgentCommand>) {
+        let Ok((_node, mut events)) =
+            DoraNode::init_from_node_id(NodeId::from("reasoner_output_moxin".to_string()))
+        else {
+            eprint!("Failed to initialize node: reasoner_output_moxin");
+            return;
+        };
 
-                                // Send response to frontend
-                                sender
-                                    .send(received_string.to_string())
+        let Ok((mut node, _events)) =
+            DoraNode::init_from_node_id(NodeId::from("reasoner_task_input".to_string()))
+        else {
+            eprint!("Failed to initialize node: reasoner_task_input");
+            return;
+        };
+
+        loop {
+            let sender_to_frontend: mpsc::Sender<MaeAgentResponse>;
+            let current_agent: MaeAgent;
+
+            // Receive command from frontend
+            match command_receiver.recv().unwrap() {
+                MaeAgentCommand::SendTask(task, agent, tx) => {
+                    // TODO Improve how we send the task prompt and the agent file
+                    let data =
+                        StringArray::from(vec![task.trim().to_string(), agent.definition_file()]);
+
+                    node.send_output(
+                        DataId::from("reasoner_task".to_string()),
+                        MetadataParameters::default(),
+                        data,
+                    )
+                    .expect("failed to send task to reasoner");
+
+                    sender_to_frontend = tx;
+                    current_agent = agent;
+                }
+            }
+
+            dbg!(&sender_to_frontend, &current_agent);
+
+            // Listen for events from reasoner to send the response to frontend
+            '_while: while let Some(event) = events.recv() {
+                dbg!(&event);
+                match event {
+                    Event::Input {
+                        id,
+                        metadata: _,
+                        data,
+                    } => {
+                        match data.data_type() {
+                            dora_node_api::arrow::datatypes::DataType::Utf8 => {
+                                let received_string: &str =
+                                    TryFrom::try_from(&data).expect("expected string message");
+
+                                let parsed =
+                                    current_agent.parse_response(received_string.to_string());
+                                dbg!(&parsed);
+                                sender_to_frontend
+                                    .send(parsed)
                                     .expect("failed to send command");
                             }
                             _other => {
                                 println!("Received id: {}, data: {:#?}", id, data);
                             }
-                        },
-                        _other => {}
-                    }
-                }
-                // Waiting for the daemon to update ending of the dataflow.
-                std::thread::sleep(std::time::Duration::from_secs(3));
-            } else {
-                std::thread::sleep(std::time::Duration::from_secs(3));
-            }
-        }
-    }
-
-    pub fn start_sender_loop(
-        command_receiver: mpsc::Receiver<MaeAgentCommand>,
-        rx: mpsc::Receiver<String>,
-    ) {
-        if let Ok((mut node, _events)) =
-            DoraNode::init_from_node_id(NodeId::from("reasoner_task_input".to_string()))
-        {
-            loop {
-                match command_receiver.recv().unwrap() {
-                    MaeAgentCommand::SendTask(task, agent, tx) => {
-                        // TODO Improve how we send the task prompt and the agent file
-                        let data = StringArray::from(vec![
-                            task.trim().to_string(),
-                            agent.definition_file()
-                        ]);
-        
-                        let res = node.send_output(
-                            DataId::from("reasoner_task".to_string()),
-                            MetadataParameters::default(),
-                            data,
-                        );
-                        dbg!(res.unwrap());
-
-                        match rx.recv() {
-                            Ok(response) => {
-                                let parsed = agent.parse_response(response);
-                                tx.send(parsed).unwrap();
-                            }
-                            Err(err) => {
-                                println!("Error: {:?}", err);
-                            }
                         }
+
+                        // Stop listening for events after receiving the actual response
+                        break '_while;
                     }
+                    _other => {}
                 }
             }
         }
