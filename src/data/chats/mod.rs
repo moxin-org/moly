1f0312874f148e42b4c64833428486dbda3aa005
Jorge Bejar <jorge@wyeworks.com>
2024-09-11 10:32:45 -0300

    Fix crash trying to sanitize an empty conversation

--- src/data/chats/mod.rs
+++ src/data/chats/mod.rs
@@ -108,9 +108,10 @@ impl Chats {
         if let Some(chat) = self.get_current_chat() {
             chat.borrow_mut().cancel_streaming(self.backend.as_ref());
             let mut chat = self.get_current_chat().unwrap().borrow_mut();
-            let message = chat.messages.last_mut().unwrap();
-            if message.content.trim().is_empty() {
-                chat.messages.pop();
+            if let Some(message) = chat.messages.last_mut() {
+                if message.content.trim().is_empty() {
+                    chat.messages.pop();
+                }
             }
         }
     }
