160ab6f8db081c4351b2b3aff991bd5c1cfa5dec
noxware <7684329+noxware@users.noreply.github.com>
2025-02-22 03:30:02 -0300

    remove the last unsafe code from moly-kit

--- moly-kit/src/utils/events.rs
+++ moly-kit/src/utils/events.rs
@@ -1,39 +1,15 @@
-//! Tries to simplify event handling for makepad at least inside this crate.
-
-use makepad_widgets::{Action, Actions, Event};
-use std::sync::Once;
-
-static mut EMPTY: *const Vec<Action> = std::ptr::null();
-static INIT: Once = Once::new();
+use makepad_widgets::{Action, Event};
 
 pub trait EventExt {
-    /// Extract the actions from this events (if any).
-    ///
-    /// A workaround is used when the Makepad's Event is not an Actions variant
-    /// to return a 'static immutable reference to a global empty vector.
-    fn actions(&self) -> &Actions;
+    /// Returns `&[Action]` (either the event's actions or an empty fallback).
+    fn actions(&self) -> &[Action];
 }
 
 impl EventExt for Event {
-    fn actions(&self) -> &Actions {
+    fn actions(&self) -> &[Action] {
         match self {
-            Event::Actions(actions) => actions,
-            _ => unsafe {
-                // This is safe just because it's an immutable reference and the
-                // vector is empty.
-                //
-                // A non-unsafe implementation would require a thread_local storage
-                // since the Action type is not Send but that may be even dangerous
-                // as it would need to leak a vector for each thread (which should only be one
-                // but who knows). So I preferred goging unsafe here.
-
-                INIT.call_once(|| {
-                    // Leak an empty vector to get 'static reference
-                    let empty = Box::new(Vec::new());
-                    EMPTY = Box::leak(empty);
-                });
-                &*EMPTY
-            },
+            Event::Actions(actions) => actions.as_slice(),
+            _ => &[],
         }
     }
 }
