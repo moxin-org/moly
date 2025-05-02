6b1c64970c7f62eeb70a3c77571572c422168d7d
Franco Profeti <7684329+noxware@users.noreply.github.com>
2025-05-02 16:26:38 -0300

    handle set messages task at moly (#441)

--- src/chat/chat_screen.rs
+++ src/chat/chat_screen.rs
@@ -1,6 +1,6 @@
 use makepad_widgets::*;
-use moly_kit::*;
 use moly_kit::utils::asynchronous::spawn;
+use moly_kit::*;
 
 use crate::data::capture::CaptureAction;
 use crate::data::providers::ProviderType;
@@ -29,7 +29,7 @@ live_design! {
             // Shader to make the original RoundedView into a RoundedShadowView
             // (can't simply override the type of `persistent` because that removes the original children)
             clip_x:false, clip_y:false,
-                                
+
             show_bg: true,
             draw_bg: {
                 color: #fefefe
@@ -39,18 +39,18 @@ live_design! {
                 uniform shadow_color: #0001
                 uniform shadow_radius: 9.0,
                 uniform shadow_offset: vec2(0.0,-2.5)
-                                                
+
                 varying rect_size2: vec2,
                 varying rect_size3: vec2,
-                varying rect_pos2: vec2,     
-                varying rect_shift: vec2,    
+                varying rect_pos2: vec2,
+                varying rect_shift: vec2,
                 varying sdf_rect_pos: vec2,
                 varying sdf_rect_size: vec2,
-                                                
+
                 fn get_color(self) -> vec4 {
                     return self.color
                 }
-                                                
+
                 fn vertex(self) -> vec4 {
                     let min_offset = min(self.shadow_offset,vec2(0));
                     self.rect_size2 = self.rect_size + 2.0*vec2(self.shadow_radius);
@@ -59,22 +59,22 @@ live_design! {
                     self.sdf_rect_size = self.rect_size2 - vec2(self.shadow_radius * 2.0 + self.border_size * 2.0)
                     self.sdf_rect_pos = -min_offset + vec2(self.border_size + self.shadow_radius);
                     self.rect_shift = -min_offset;
-                                                                
+
                     return self.clip_and_transform_vertex(self.rect_pos2, self.rect_size3)
                 }
-                                                            
+
                 fn get_border_color(self) -> vec4 {
                     return self.border_color
                 }
-                                                    
+
                 fn pixel(self) -> vec4 {
-                                                                    
+
                     let sdf = Sdf2d::viewport(self.pos * self.rect_size3)
                     sdf.box(
                         self.sdf_rect_pos.x,
                         self.sdf_rect_pos.y,
                         self.sdf_rect_size.x,
-                        self.sdf_rect_size.y, 
+                        self.sdf_rect_size.y,
                         max(1.0, self.border_radius)
                     )
                     if sdf.shape > -1.0{
@@ -83,7 +83,7 @@ live_design! {
                         let v = GaussShadow::rounded_box_shadow(vec2(m) + o, self.rect_size2+o, self.pos * (self.rect_size3+vec2(m)), self.shadow_radius*0.5, self.border_radius*2.0);
                         sdf.clear(self.shadow_color*v)
                     }
-                                                                        
+
                     sdf.fill_keep(self.get_color())
                     if self.border_size > 0.0 {
                         sdf.stroke(self.get_border_color(), self.border_size)
@@ -121,11 +121,11 @@ live_design! {
 
         // TODO: Add chat params back in, only when the model is a local model (MolyServer)
         // currenlty MolyKit does not support chat params
-        // 
+        //
         // <View> {
         //     width: Fit,
         //     height: Fill,
-        // 
+        //
         //     chat_params = <ChatParams> {}
         // }
     }
@@ -147,7 +147,7 @@ pub struct ChatScreen {
 }
 
 impl LiveHook for ChatScreen {
-    fn after_new_from_doc(&mut self, _cx:&mut Cx) {
+    fn after_new_from_doc(&mut self, _cx: &mut Cx) {
         self.prompt_input(id!(chat.prompt)).write().disable();
     }
 }
@@ -156,7 +156,7 @@ impl Widget for ChatScreen {
     fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
         self.ui_runner().handle(cx, event, scope, self);
         self.widget_match_event(cx, event, scope);
-  
+
         // TODO This check is actually copied from Makepad view.rs file
         // It's not clear why it's needed here, but without this line
         // the "View all files" link in Discover section does not work after visiting the chat screen
@@ -207,13 +207,18 @@ impl WidgetMatchEvent for ChatScreen {
             match action.cast() {
                 ChatAction::Start(bot_id) => {
                     store.chats.create_empty_chat(Some(bot_id.clone()));
-                    self.messages(id!(chat.messages)).write().set_messages(vec![], true);
+                    self.messages(id!(chat.messages))
+                        .write()
+                        .set_messages(vec![], true);
                     self.chat(id!(chat)).write().bot_id = Some(bot_id.clone());
-                    self.model_selector(id!(model_selector)).set_currently_selected_model(cx, Some(bot_id));
+                    self.model_selector(id!(model_selector))
+                        .set_currently_selected_model(cx, Some(bot_id));
                     // self.focus_on_prompt_input_pending = true;
                 }
                 ChatAction::StartWithoutEntity => {
-                    self.messages(id!(chat.messages)).write().set_messages(vec![], true);
+                    self.messages(id!(chat.messages))
+                        .write()
+                        .set_messages(vec![], true);
                     // self.focus_on_prompt_input_pending = true;
                 }
                 _ => {}
@@ -228,7 +233,8 @@ impl WidgetMatchEvent for ChatScreen {
                         if let ChatTask::InsertMessage(_index, message) = task {
                             let message = message.clone();
                             ui.defer_with_redraw(move |_me, _cx, scope| {
-                                let current_chat = scope.data.get::<Store>().unwrap().chats.get_current_chat();
+                                let current_chat =
+                                    scope.data.get::<Store>().unwrap().chats.get_current_chat();
                                 if let Some(store_chat) = current_chat {
                                     let mut store_chat = store_chat.borrow_mut();
                                     let mut new_message = message.clone();
@@ -242,16 +248,19 @@ impl WidgetMatchEvent for ChatScreen {
 
                         // Handle updated Bot messages
                         // UpdateMessage tasks mean that a bot message has been updated, either a User edit or a Bot message delta from the stream
-                        // We fetch the current chat from the store and update the corresponding message, or insert it if it's not present 
+                        // We fetch the current chat from the store and update the corresponding message, or insert it if it's not present
                         // (if it's the first chunk from the bot message)
                         if let ChatTask::UpdateMessage(index, message) = task {
                             let message = message.clone();
                             let index = index.clone();
                             ui.defer_with_redraw(move |_me, _cx, scope| {
-                                let current_chat = scope.data.get::<Store>().unwrap().chats.get_current_chat();
+                                let current_chat =
+                                    scope.data.get::<Store>().unwrap().chats.get_current_chat();
                                 if let Some(store_chat) = current_chat {
                                     let mut store_chat = store_chat.borrow_mut();
-                                    if let Some(message_to_update) = store_chat.messages.get_mut(index) {
+                                    if let Some(message_to_update) =
+                                        store_chat.messages.get_mut(index)
+                                    {
                                         message_to_update.content = message.content.clone();
                                         message_to_update.is_writing = false;
                                     } else {
@@ -259,6 +268,19 @@ impl WidgetMatchEvent for ChatScreen {
                                         new_message.is_writing = false;
                                         store_chat.messages.push(new_message);
                                     }
+                                    store_chat.save();
+                                }
+                            });
+                        }
+
+                        if let ChatTask::SetMessages(messages) = task {
+                            let messages = messages.clone();
+                            ui.defer_with_redraw(move |_me, _cx, scope| {
+                                let current_chat =
+                                    scope.data.get::<Store>().unwrap().chats.get_current_chat();
+                                if let Some(store_chat) = current_chat {
+                                    let mut store_chat = store_chat.borrow_mut();
+                                    store_chat.messages = messages;
                                     store_chat.save();
                                 }
                             });
@@ -272,17 +294,6 @@ impl WidgetMatchEvent for ChatScreen {
                                 me.redraw(cx);
                             });
                         }
-
-                        // TODO(MolyKit): Handle regenerate response?
-                        //     ChatLineAction::Edit(id, updated, regenerate) => {
-                        //         if regenerate {
-                        //             self.send_message(cx, scope, updated, Some(id));
-                        //             return;
-                        //         } else {
-                        //             store.edit_chat_message(id, updated);
-                        //         }
-                        //         self.redraw(cx);
-                        //     }
                     }
                 });
             });
@@ -293,14 +304,19 @@ impl WidgetMatchEvent for ChatScreen {
                     let current_chat = store.chats.get_current_chat();
 
                     if let Some(chat) = current_chat {
-                        store.preferences.set_current_chat_model(chat.borrow().associated_bot.clone());
+                        store
+                            .preferences
+                            .set_current_chat_model(chat.borrow().associated_bot.clone());
 
                         // Load messages from history into the messages widget
-                        self.messages(id!(chat.messages)).write().set_messages(chat.borrow().messages.clone(), true);
+                        self.messages(id!(chat.messages))
+                            .write()
+                            .set_messages(chat.borrow().messages.clone(), true);
 
                         // Set the chat's associated model in the model selector
                         if let Some(bot_id) = &chat.borrow().associated_bot {
-                            self.model_selector(id!(model_selector)).set_currently_selected_model(cx, Some(bot_id.clone()));
+                            self.model_selector(id!(model_selector))
+                                .set_currently_selected_model(cx, Some(bot_id.clone()));
                             self.chat(id!(chat)).write().bot_id = Some(bot_id.clone());
                         }
 
@@ -311,13 +327,14 @@ impl WidgetMatchEvent for ChatScreen {
             }
 
             // Handle Context Capture
-            if let CaptureAction::Capture { event } = action.cast(){
-                self.prompt_input(id!(prompt)).write().set_text(cx, event.contents());
+            if let CaptureAction::Capture { event } = action.cast() {
+                self.prompt_input(id!(prompt))
+                    .write()
+                    .set_text(cx, event.contents());
             }
         }
     }
 }
-
 
 impl ChatScreen {
     fn create_bot_repo(&mut self, _cx: &mut Cx, scope: &mut Scope) {
@@ -329,14 +346,17 @@ impl ChatScreen {
             for provider in store.chats.providers.iter() {
                 match provider.1.provider_type {
                     ProviderType::OpenAI | ProviderType::MolyServer => {
-                        if provider.1.enabled && (provider.1.api_key.is_some() || provider.1.url.starts_with("http://localhost")) {
+                        if provider.1.enabled
+                            && (provider.1.api_key.is_some()
+                                || provider.1.url.starts_with("http://localhost"))
+                        {
                             let mut new_client = OpenAIClient::new(provider.1.url.clone());
                             if let Some(key) = provider.1.api_key.as_ref() {
                                 let _ = new_client.set_key(&key);
                             }
                             multi_client.add_client(Box::new(new_client));
                         }
-                    },
+                    }
                     ProviderType::MoFa => {
                         // For MoFa we don't require an API key
                         if provider.1.enabled {
@@ -346,7 +366,7 @@ impl ChatScreen {
                             }
                             multi_client.add_client(Box::new(new_client));
                         }
-                    },
+                    }
                     ProviderType::DeepInquire => {
                         let mut new_client = DeepInquireClient::new(provider.1.url.clone());
                         if let Some(key) = provider.1.api_key.as_ref() {
@@ -359,17 +379,17 @@ impl ChatScreen {
 
             multi_client
         };
-    
+
         let mut repo: BotRepo = multi_client.into();
         self.chat(id!(chat)).write().bot_repo = Some(repo.clone());
 
         self.creating_bot_repo = true;
 
         let ui = self.ui_runner();
-            spawn(async move {
-                repo.load().await;
+        spawn(async move {
+            repo.load().await;
 
-                ui.defer_with_redraw(move |me, _cx, _scope| {
+            ui.defer_with_redraw(move |me, _cx, _scope| {
                 me.should_load_repo_to_store = true;
                 me.creating_bot_repo = false;
             });
