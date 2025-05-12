e3c16b752d6bfc0896028a8ceedc10d6c891e73c
Franco Profeti <7684329+noxware@users.noreply.github.com>
2025-05-12 16:37:48 -0300

    Moly book (#460)
    * add moly kit readme
    
    * grammar and spell
    
    * initial docs idea
    
    * a good quickstart
    
    * multi provider docs
    
    * some notes
    
    * integration docs still wip
    
    * continue writing integration doc
    
    * grammar and stuff
    
    * web support doc
    
    * added prerequisites to web doc
    
    * clients doc almost there
    
    * tokio warning and grammar on clients
    
    * custom content doc
    
    * docs to mdbook
    
    * adjust book toml
    
    * remove index.md from book
    
    * github page attempt
    
    * update readme
    
    * remove docs dir
    
    * try changing page path
    
    * fix doc path
    
    * sync lib.rs with readme
    
    * added makepad link
    
    * publish page only on main branch push

---
+++ book/src/advanced.md
@@ -0,0 +1,5 @@
+# Advanced
+
+This section covers advanced topics that are typically not necessary unless you
+need to extend Moly Kit beyond its built-in features. These chapters will guide
+you through more complex customizations and integrations.
--- moly-kit/src/lib.rs
+++ moly-kit/src/lib.rs
@@ -1,84 +1,20 @@
-//! # Basic usage
+//! # Description
 //!
-//! ## Installation
-//! Add MolyKit to your dependencies.
-//! ```toml
-//! moly-kit = { version = "*", features = ["full"] }
-//! ```
+//! Moly Kit is a Rust crate containing widgets and utilities to streamline the development
+//! of artificial intelligence applications for the [Makepad](https://github.com/makepad/makepad)
+//! framework.
 //!
-//! ## DSL
-//! Import the batteries-included [Chat] widget.
-//! ```
-//! use moly_kit::widgets::chat::Chat;
-//! ```
-//! Then, add it somewhere in your parent widget.
-//! ```
-//! chat = <Chat> {}
-//! ```
+//! # Features
 //!
-//! ## Rust
-//! We need to give the chat a [BotRepo], which allows [Chat] to pull information
-//! about the available bots and send messages to them.
+//! - ⚡️ Low-config `Chat` widget that works almost out of the box.
+//! - 🔧 Customize and integrate behavior of `Chat` into your own app.
+//! - 🎨 Customize appearance thanks to Makepad DSL overrides.
+//! - 📞 Built-in OpenAI-compatible client.
+//! - 🧩 Extensible with your own clients and custom message contents.
+//! - 🌎 Web support.
 //!
-//! A [BotRepo] can be directly created from a [BotClient], which interacts
-//! with (mostly remote) bot providers like OpenAI, Ollama, OpenRouter,
-//! Moly Server, MoFa, etc.
-//!
-//! An OpenAI compatible client comes out-of-the-box with MolyKit, [OpenAIClient].
-//!
-//! So, add the following somewhere appropriate (like in `after_new_from_doc`
-//! from Makepad) to give [Chat] its [BotRepo]:
-//! ```rust
-//! use moly_kit::*
-//!
-//! let mut client = OpenAIClient::new("https://api.openai.com".into());
-//! client.set_key("<YOUR_KEY>".into());
-//!
-//! let repo = BotRepo::from(client);
-//!
-//! self.chat(id!(chat)).write_with(|chat| {
-//!   chat.bot_repo = repo;
-//! })
-//! ```
-//!
-//! # Advanced usage
-//! [Chat] is by default a very automatic widget, but it exposes its lifecycle
-//! through a "hook" mechanism. This mechanism allows you to:
-//! - Know that something will happen.
-//! - Know that something already happened.
-//! - Abort something before it happens.
-//! - Replace that aborted thing with something else.
-//! - Inject tasks to be executed by the [Chat].
-//! - Change the payload of a task affecting the operation when executed.
-//!
-//! TODO: Continue writing and give examples.
-//!
-//! # Web support
-//!
-//! If you want to use the built-in web support (the `async-web` flag), you may need
-//! to do the following at `main.rs`:
-//!
-//! ```rust
-//! #[cfg(target_arch = "wasm32")]
-//! use wasm_bindgen::prelude::*;
-//!
-//! #[cfg(target_arch = "wasm32")]
-//! fn main() {
-//!     your_application_lib::app::app_main()
-//! }
-//!
-//! #[cfg(not(target_arch = "wasm32"))]
-//! #[tokio::main]
-//! async fn main() {
-//!     your_application_lib::app::app_main()
-//! }
-//! ```
-//!
-//! And then run your app with `wasm-bindgen` support like this:
-//!
-//! ```sh
-//! cargo makepad wasm --bindgen run -p your_application_package
-//! ```
+//! To learn how to use and integrate Moly Kit into your own Makepad app, read the
+//! [documentation](https://moxin-org.github.io/moly).
 
 pub mod clients;
 pub mod protocol;
